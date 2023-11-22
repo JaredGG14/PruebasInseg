3fd999fe1da2352fdaefbbd24f63237bf949397f
Checotheg0d <cm85390@gmail.com>
2023-11-22 00:59:19 -0600

    commit

--- login.php
+++ login.php
@@ -9,23 +9,36 @@ $dbname = "practice4";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-    die("Conection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
 $user = $_POST['username'];
 $pass = $_POST['password'];
 
-//Inseguro
 $sql = "SELECT * FROM users WHERE username = '$user' AND password = '$pass'";
-//Seguro
-//$sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+
 $result = $conn->query($sql);
-
 if ($result->num_rows > 0) {
-    echo "¡Login succesful!";
+    echo "Login succesful!";
 } else {
-    echo "User or password wrong.";
+    echo "Wrong ser or password.";
 }
-
 $conn->close();
+
+/*
+$stmt = $conn->prepare("SELECT * FROM users WHERE username = ? AND password = ?");
+$stmt->bind_param("ss", $user, $pass);
+$stmt->execute();
+
+$stmt->store_result();
+
+if ($stmt->num_rows > 0) {
+    echo "Login successful!";
+} else {
+    echo "Wrong user or password.";
+}
+// Cerrar la sentencia y la conexión
+$stmt->close();
+*/
+
 ?>
